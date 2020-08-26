e0779e8d3993a347d54b10799a9e6bb071e423cc
Andre Mata <=>
2020-08-25 22:33:07 -0300

    Projeto criado

---
+++ src/main/java/com/andremata/projetospringbootjava/ProjetoSpringBootJavaApplication.java
@@ -0,0 +1,12 @@
+package com.andremata.projetospringbootjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringBootJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringBootJavaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/andremata/projetospringbootjava/ProjetoSpringBootJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andremata.projetospringbootjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringBootJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
