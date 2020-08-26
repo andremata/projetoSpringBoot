d20837b62c667b93f0b0291768352a3f28df1555
Andre Mata <=>
2020-08-26 18:59:55 -0300

    Criação da entidade Produto e associação com Categoria

--- src/main/java/com/andremata/projetospringbootjava/ProjetoSpringBootJavaApplication.java
+++ src/main/java/com/andremata/projetospringbootjava/ProjetoSpringBootJavaApplication.java
@@ -1,7 +1,6 @@
 package com.andremata.projetospringbootjava;
 
 import java.util.Arrays;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -9,22 +8,40 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.andremata.projetospringbootjava.domain.Categoria;
+import com.andremata.projetospringbootjava.domain.Produto;
 import com.andremata.projetospringbootjava.repositories.CategoriaRepository;
+import com.andremata.projetospringbootjava.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class ProjetoSpringBootJavaApplication implements CommandLineRunner 	{
 
 	@Autowired
-	private CategoriaRepository repository;
+	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoSpringBootJavaApplication.class, args);
 	}
 
 	@Override
-	public void run(String... args) throws Exception {		
-		List<Categoria> categorias = Arrays.asList(new Categoria(1, "Informática"), new Categoria(2, "Escritório"));
+	public void run(String... args) throws Exception {
+		Categoria c1 = new Categoria(null, "Informática");
+		Categoria c2 = new Categoria(null, "Escritório");
 		
-		repository.saveAll(categorias);
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+				
+		c1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		c2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(c1));
+		p2.getCategorias().addAll(Arrays.asList(c1, c2));
+		p3.getCategorias().addAll(Arrays.asList(c1));
+		
+		categoriaRepository.saveAll(Arrays.asList(c1, c2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 }
