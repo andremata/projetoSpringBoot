33d8c365181962b5aeb5395bc29485bd48d0ab43
Andre Mata <=>
2020-08-25 23:27:55 -0300

    Criado classe de dominio e REST testado

---
+++ src/main/java/com/andremata/projetospringbootjava/resources/CategoriaResource.java
@@ -0,0 +1,23 @@
+package com.andremata.projetospringbootjava.resources;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.andremata.projetospringbootjava.domain.Categoria;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaResource {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public List<Categoria> consultar() {
+		
+		List<Categoria> categorias = Arrays.asList(new Categoria(1, "Informática"), new Categoria(2, "Escrotório"));
+		
+		return categorias;
+	}
+}
